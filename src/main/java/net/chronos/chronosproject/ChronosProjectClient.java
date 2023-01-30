443df337ba78851c5ea2bb026a9db4a8cf6b2997
Olegaterstvo <olegaterstvo@duck.com>
2023-01-30 12:59:03 +0300

    Initial commit

---
+++ src/main/java/net/chronos/chronosproject/ChronosProjectClient.java
@@ -0,0 +1,10 @@
+package net.chronos.chronosproject;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChronosProjectClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
