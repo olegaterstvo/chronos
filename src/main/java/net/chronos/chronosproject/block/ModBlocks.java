d9cdddbc9e0532c3865a9248b21bc4430c8b2369
Olegaterstvo <olegaterstvo@duck.com>
2023-01-31 01:31:58 +0300

    concrete stairs

--- src/main/java/net/chronos/chronosproject/block/ModBlocks.java
+++ src/main/java/net/chronos/chronosproject/block/ModBlocks.java
@@ -60,11 +60,42 @@ public class ModBlocks {
     public static final Block PIPSKIY_CONCRETE_SLAB = registerBlock("pipskiy_concrete_slab",
             new SlabBlock(FabricBlockSettings.of(Material.STONE).strength(1.8f).hardness(1.8F)), ModItemGroup.CHRONOSPROJECT);
 
-
-
-
     public static final Block WHITE_CONCRETE_STAIRS = registerBlock("white_concrete_stairs",
             new ModStairsBlock(Blocks.WHITE_CONCRETE.getDefaultState(),FabricBlockSettings.of(Material.STONE).strength(1.8f).hardness(1.8F)), ModItemGroup.CHRONOSPROJECT);
+    public static final Block ORANGE_CONCRETE_STAIRS = registerBlock("orange_concrete_stairs",
+            new ModStairsBlock(Blocks.ORANGE_CONCRETE.getDefaultState(), FabricBlockSettings.of(Material.STONE).strength(1.8f).hardness(1.8F)), ModItemGroup.CHRONOSPROJECT);
+    public static final Block MAGENTA_CONCRETE_STAIRS = registerBlock("magenta_concrete_stairs",
+            new ModStairsBlock(Blocks.MAGENTA_CONCRETE.getDefaultState(), FabricBlockSettings.of(Material.STONE).strength(1.8f).hardness(1.8F)), ModItemGroup.CHRONOSPROJECT);
+    public static final Block LIGHT_BLUE_CONCRETE_STAIRS = registerBlock("light_blue_concrete_stairs",
+            new ModStairsBlock(Blocks.LIGHT_BLUE_CONCRETE.getDefaultState(), FabricBlockSettings.of(Material.STONE).strength(1.8f).hardness(1.8F)), ModItemGroup.CHRONOSPROJECT);
+    public static final Block YELLOW_CONCRETE_STAIRS = registerBlock("yellow_concrete_stairs",
+            new ModStairsBlock(Blocks.YELLOW_CONCRETE.getDefaultState(), FabricBlockSettings.of(Material.STONE).strength(1.8f).hardness(1.8F)), ModItemGroup.CHRONOSPROJECT);
+    public static final Block LIME_CONCRETE_STAIRS = registerBlock("lime_concrete_stairs",
+            new ModStairsBlock(Blocks.LIME_CONCRETE.getDefaultState(), FabricBlockSettings.of(Material.STONE).strength(1.8f).hardness(1.8F)), ModItemGroup.CHRONOSPROJECT);
+    public static final Block PINK_CONCRETE_STAIRS = registerBlock("pink_concrete_stairs",
+            new ModStairsBlock(Blocks.PINK_CONCRETE.getDefaultState(), FabricBlockSettings.of(Material.STONE).strength(1.8f).hardness(1.8F)), ModItemGroup.CHRONOSPROJECT);
+    public static final Block GRAY_CONCRETE_STAIRS = registerBlock("gray_concrete_stairs",
+            new ModStairsBlock(Blocks.GRAY_CONCRETE.getDefaultState(), FabricBlockSettings.of(Material.STONE).strength(1.8f).hardness(1.8F)), ModItemGroup.CHRONOSPROJECT);
+    public static final Block LIGHT_GRAY_CONCRETE_STAIRS = registerBlock("light_gray_concrete_stairs",
+            new ModStairsBlock(Blocks.LIGHT_GRAY_CONCRETE.getDefaultState(), FabricBlockSettings.of(Material.STONE).strength(1.8f).hardness(1.8F)), ModItemGroup.CHRONOSPROJECT);
+    public static final Block CYAN_CONCRETE_STAIRS = registerBlock("cyan_concrete_stairs",
+            new ModStairsBlock(Blocks.CYAN_CONCRETE.getDefaultState(), FabricBlockSettings.of(Material.STONE).strength(1.8f).hardness(1.8F)), ModItemGroup.CHRONOSPROJECT);
+    public static final Block PURPLE_CONCRETE_STAIRS = registerBlock("purple_concrete_stairs",
+            new ModStairsBlock(Blocks.PURPLE_CONCRETE.getDefaultState(), FabricBlockSettings.of(Material.STONE).strength(1.8f).hardness(1.8F)), ModItemGroup.CHRONOSPROJECT);
+    public static final Block BLUE_CONCRETE_STAIRS = registerBlock("blue_concrete_stairs",
+            new ModStairsBlock(Blocks.BLUE_CONCRETE.getDefaultState(), FabricBlockSettings.of(Material.STONE).strength(1.8f).hardness(1.8F)), ModItemGroup.CHRONOSPROJECT);
+    public static final Block BROWN_CONCRETE_STAIRS = registerBlock("brown_concrete_stairs",
+            new ModStairsBlock(Blocks.BROWN_CONCRETE.getDefaultState(), FabricBlockSettings.of(Material.STONE).strength(1.8f).hardness(1.8F)), ModItemGroup.CHRONOSPROJECT);
+    public static final Block GREEN_CONCRETE_STAIRS = registerBlock("green_concrete_stairs",
+            new ModStairsBlock(Blocks.GREEN_CONCRETE.getDefaultState(), FabricBlockSettings.of(Material.STONE).strength(1.8f).hardness(1.8F)), ModItemGroup.CHRONOSPROJECT);
+    public static final Block RED_CONCRETE_STAIRS = registerBlock("red_concrete_stairs",
+            new ModStairsBlock(Blocks.RED_CONCRETE.getDefaultState(), FabricBlockSettings.of(Material.STONE).strength(1.8f).hardness(1.8F)), ModItemGroup.CHRONOSPROJECT);
+    public static final Block BLACK_CONCRETE_STAIRS = registerBlock("black_concrete_stairs",
+            new ModStairsBlock(Blocks.BLACK_CONCRETE.getDefaultState(), FabricBlockSettings.of(Material.STONE).strength(1.8f).hardness(1.8F)), ModItemGroup.CHRONOSPROJECT);
+    public static final Block CHRONOS_CONCRETE_STAIRS = registerBlock("chronos_concrete_stairs",
+            new ModStairsBlock(ModBlocks.CHRONOS_CONCRETE.getDefaultState(), FabricBlockSettings.of(Material.STONE).strength(1.8f).hardness(1.8F)), ModItemGroup.CHRONOSPROJECT);
+    public static final Block PIPSKIY_CONCRETE_STAIRS = registerBlock("pipskiy_concrete_stairs",
+            new ModStairsBlock(ModBlocks.PIPSKIY_CONCRETE.getDefaultState(), FabricBlockSettings.of(Material.STONE).strength(1.8f).hardness(1.8F)), ModItemGroup.CHRONOSPROJECT);
 
 
 
