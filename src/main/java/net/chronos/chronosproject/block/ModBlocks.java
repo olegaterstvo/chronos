34264e35ccf1947bf27036eedb52386b63cc5f09
Olegaterstvo <olegaterstvo@duck.com>
2023-01-30 18:55:07 +0300

    meh

--- src/main/java/net/chronos/chronosproject/block/ModBlocks.java
+++ src/main/java/net/chronos/chronosproject/block/ModBlocks.java
@@ -1,22 +1,33 @@
 package net.chronos.chronosproject.block;
 
 import net.chronos.chronosproject.ChronosProject;
+import net.chronos.chronosproject.block.custom.ModStairsBlock;
 import net.chronos.chronosproject.item.ModItemGroup;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.Material;
+import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModBlocks {
     public static final Block CHRONOS_CONCRETE = registerBlock("chronos_concrete",
-            new Block(FabricBlockSettings.of(Material.STONE).strength(4f)), ModItemGroup.CHRONOSPROJECT);
+            new Block(FabricBlockSettings.of(Material.STONE).strength(1.8f).hardness(1.8F)), ModItemGroup.CHRONOSPROJECT);
     public static final Block PIPSKIY_CONCRETE = registerBlock("pipskiy_concrete",
-            new Block(FabricBlockSettings.of(Material.STONE).strength(4f)), ModItemGroup.CHRONOSPROJECT);
+            new Block(FabricBlockSettings.of(Material.STONE).strength(1.8f).hardness(1.8F)), ModItemGroup.CHRONOSPROJECT);
+    public static final Block CHRONOS_CONCRETE_POWDER = registerBlock("chronos_concrete_powder",
+            new FallingBlock(FabricBlockSettings.of(Material.SOIL).sounds(BlockSoundGroup.SAND).strength(0.5f).hardness(0.5F)), ModItemGroup.CHRONOSPROJECT);
+    public static final Block PIPSKIY_CONCRETE_POWDER = registerBlock("pipskiy_concrete_powder",
+            new FallingBlock(FabricBlockSettings.of(Material.SOIL).sounds(BlockSoundGroup.SAND).strength(0.5f).hardness(0.5F)), ModItemGroup.CHRONOSPROJECT);
+    public static final Block WHITE_CONCRETE_SLAB = registerBlock("white_concrete_slab",
+            new SlabBlock(FabricBlockSettings.of(Material.STONE).strength(1.8f).hardness(1.8F)), ModItemGroup.CHRONOSPROJECT);
+    public static final Block WHITE_CONCRETE_STAIRS = registerBlock("white_concrete_stairs",
+            new ModStairsBlock(ModBlocks.CHRONOS_CONCRETE.getDefaultState(),FabricBlockSettings.of(Material.STONE).strength(1.8f).hardness(1.8F)), ModItemGroup.CHRONOSPROJECT);
+
+
 
 
     private static Block registerBlock(String name, Block block, ItemGroup tab){
