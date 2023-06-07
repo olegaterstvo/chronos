9513e5495a94c9cd8a107ff56f90c8a6f652b38b
Olegaterstvo <olegaterstvo@duck.com>
2023-06-08 00:12:55 +0300

    forge initial commit

--- src/main/java/net/chronos/chronosproject/ChronosProject.java
+++ src/main/java/net/chronos/chronosproject/ChronosProject.java
@@ -1,21 +1,44 @@
 package net.chronos.chronosproject;
 
-import net.chronos.chronosproject.block.ModBlocks;
-import net.chronos.chronosproject.item.ModItemGroup;
-import net.chronos.chronosproject.item.ModItems;
-import net.fabricmc.api.ModInitializer;
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class ChronosProject implements ModInitializer {
-	public static final String MOD_ID = "chronosproject";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+import java.util.stream.Collectors;
 
-	@Override
-	public void onInitialize() {
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(ChronosProject.MOD_ID)
+public class ChronosProject
+{
+    public static final String MOD_ID = "chronosproject";
 
-		ModItemGroup.registerItemGroups();
-		ModItems.registerModItems();
-		ModBlocks.registerModBlocks();
-	}
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public ChronosProject()
+    {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
 }
